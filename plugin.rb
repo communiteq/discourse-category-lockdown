e6212d100bcd3c3ff800f8740f239a6f61617a82
communiteq <richard@communiteq.com>
2025-03-09 08:45:12 +0100

    Avoid UnsafeRedirectError
    When redirecting to an external domain, `UnsafeRedirectError`  happened.
    
    See https://coop.pavilion.tech/t/unsafe-redirect-error/4108

--- plugin.rb
+++ plugin.rb
@@ -124,7 +124,7 @@ after_initialize do
       if request.format.json?
         render_json_dump(response, status: 402)
       else
-        redirect_to path(response[:redirect_url]), status: :moved_permanently
+        redirect_to path(response[:redirect_url]), status: :moved_permanently, allow_other_host: true
       end
 
     end
